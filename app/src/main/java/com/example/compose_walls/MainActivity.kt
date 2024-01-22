c3e11f3b56bb48963bb0d0d8f58ea88fae26645b
Augustinekan <augustinekanyuira0@gmail.com>
2024-01-22 11:01:38 +0300

    Display Wallpaper Categories

--- app/src/main/java/com/example/compose_walls/MainActivity.kt
+++ app/src/main/java/com/example/compose_walls/MainActivity.kt
@@ -1,18 +1,39 @@
 package com.example.compose_walls
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.example.compose_walls.ui.theme.Compose_wallsTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +43,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeBody("Compose Body")
                 }
             }
         }
     }
 }
 
+
+@ExperimentalMaterial3Api
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ComposeBody(name: String, modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            CustomTopAppBar()
+        },
+        content = { innerPadding ->
+            Surface (modifier = Modifier.padding(innerPadding)){
+                WallpaperCategoriesList()
+
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
+
+
+@ExperimentalMaterial3Api
 @Composable
-fun GreetingPreview() {
-    Compose_wallsTheme {
-        Greeting("Android")
-    }
+fun CustomTopAppBar(){
+    TopAppBar(
+        title = {
+            Text(
+                "Offline Wallpapers",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Favorite,
+                    contentDescription = "Localized description"
+                )
+            }
+        }
+    )
 }
---
+++ app/src/main/java/com/example/compose_walls/WallpaperCategories.kt
@@ -0,0 +1,80 @@
+package com.example.compose_walls
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WallpaperCategoriesList() {
+    LazyColumn{
+        items(getWallpaperCategories()){
+            category ->
+            WallpaperCategoryTile(category = category)
+        }
+    }
+      //List<WallpaperCategory> categories = getWallpaperCategories();
+}
+
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WallpaperCategoryTile(category:WallpaperCategory, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(MaterialTheme.colorScheme.background),
+        onClick = {
+            Log.d("Click", "CardExample: Card Click")
+
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            BasicText(text = category.title, style = MaterialTheme.typography.titleMedium)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CenteredTextCardPreview() {
+    WallpaperCategoriesList()
+}
+
+class WallpaperCategory(title: String){
+   var title: String = title
+    //Category constructor(firstName: String) { /*...*/ }
+
+}
+fun getWallpaperCategories() = listOf( Abstract, Nature, Horror, Flowers)
+
+val Abstract = WallpaperCategory(title = "Abstract");
+val Nature = WallpaperCategory(title = "Nature");
+val Horror = WallpaperCategory(title = "Horror");
+val Flowers = WallpaperCategory(title = "Flowers ");
