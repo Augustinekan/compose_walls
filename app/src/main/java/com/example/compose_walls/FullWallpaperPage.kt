4357248335651b117726ba41e1c3f228d5a8fdbb
Augustinekan <augustinekanyuira0@gmail.com>
2024-02-06 22:40:00 +0300

    set as wallpaper

--- app/src/main/java/com/example/compose_walls/FullWallpaperPage.kt
+++ app/src/main/java/com/example/compose_walls/FullWallpaperPage.kt
@@ -1,5 +1,8 @@
 package com.example.compose_walls
 
+import android.app.WallpaperManager
+import android.content.Context
+import android.graphics.drawable.BitmapDrawable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,6 +20,7 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,11 +28,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.compose_walls.utils.ImageUtils
@@ -63,29 +71,64 @@ fun FullWallpaperPage(navController: NavController, wallpaper: String) {
                             .clip(RoundedCornerShape(0.dp))
 
                     )
-            if(showBottomSheet)
-            ModalBottomSheet(
-                onDismissRequest = {
-                    showBottomSheet = false
-                },
-                sheetState = sheetState
-            ) {
-                // Sheet content
-               FullWallpaperBottomSheet()
+            if(showBottomSheet) {
+                ModalBottomSheet(
+                    containerColor = Color.Black.copy(alpha = 0.6f),
+                    onDismissRequest = {
+                        showBottomSheet = false
+                    },
+                    sheetState = sheetState
+                ) {
+                    // Sheet content
+                    FullWallpaperBottomSheet(navController, wallpaper)
 
+                }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FullWallpaperBottomSheet() {
-Box(modifier = Modifier
-    .padding(16.dp)
-    .fillMaxWidth()) {
+fun FullWallpaperBottomSheet(navController: NavController, wallpaper: String) {
+    val sheetState = rememberModalBottomSheetState()
+
+
+
+    val coroutineScope = rememberCoroutineScope()
+    val hideModalBottomSheet: () -> Unit =
+        { coroutineScope.launch { sheetState.hide() } }
+
+var local = LocalContext.current;
+
+    var ct = LocalContext.current
+    fun setAsWallpaper(wallpaper: String) {
+
+        val resources = ct.resources
+        val drawableId = resources.getIdentifier(wallpaper, "drawable", ct.packageName)
+        if (drawableId != 0) {
+            val drawable = ContextCompat.getDrawable(ct, drawableId)
+            val bitmap = (drawable as BitmapDrawable).bitmap
+
+            WallpaperManager.getInstance(ct).setBitmap(bitmap)
+
+            // Display optional preview
+            // Image(bitmap = bitmap.asImageBitmap(), contentDescription = "Wallpaper Preview")
+        } else {
+
+        }
+    }
+
+
+    Box(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
     Column {
-        Row (horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth().padding(top = 2.dp),){
+        Row (horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 2.dp),){
             Button(onClick = {
+               hideModalBottomSheet()
 //            scope.launch { sheetState.hide() }.invokeOnCompletion {
 //                if (!sheetState.isVisible) {
 //                    showBottomSheet = false
@@ -95,6 +138,9 @@ Box(modifier = Modifier
                 Text(text = "Download")
             }
             Button(onClick = {
+               hideModalBottomSheet()
+
+                setAsWallpaper(wallpaper)
 //            scope.launch { sheetState.hide() }.invokeOnCompletion {
 //                if (!sheetState.isVisible) {
 //                    showBottomSheet = false
@@ -104,15 +150,19 @@ Box(modifier = Modifier
                 Text(text = "Set Wallpaper")
             }
         }
-        Text(text = "This is a sheet")
+        //Text(text = "This is a sheet")
 
         Box(
-            modifier = Modifier.height(32.dp)
+            modifier = Modifier.height(62.dp)
         ){}
     }
 
 }
 }
+
+
+
+
 
 @Preview
 @Composable
@@ -125,7 +175,7 @@ fun FullWallpaperPagePreview() {
 @Composable
 fun FullWallpaperPageBottomSheetPreview() {
     val navController = rememberNavController();
-    FullWallpaperBottomSheet()
+    FullWallpaperBottomSheet(navController = navController, wallpaper = "volcano")
 }
 
 
